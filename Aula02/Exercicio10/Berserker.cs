a412ae025ec31049d08e9b9ac1e171b278cdfb86
Nuno Fachada <nfachada@laseeb.org>
2018-10-11 19:47:10 +0100

    Adicionar resolução do exercício 10 da aula 2

---
+++ Aula02/Exercicio10/Berserker.cs
@@ -0,0 +1,66 @@
+﻿namespace Exercicio10
+{
+
+    /// <summary>
+    /// Class that represents the Berserker player class in Fortnite.
+    /// </summary>
+    public class Berserker : FNPlayer
+    {
+
+        /// <summary>
+        /// A property representing the player's health.
+        /// </summary>
+        public override float Health
+        {
+
+            // This is the property's `get` block
+            get { return health; }
+
+            // This is the property's `set` block; the Berserker class never
+            // dies and can have health up to 2 times the maximum
+            protected set
+            {
+                // `value` is a special variable available in a property `set`
+                // block
+                if (value <= 0)
+                {
+                    // If health is zero or less, set health to one; Berserkers
+                    // never die
+                    health = 1;
+                }
+                else if (value >= 2 * MaxHealth)
+                {
+                    // If health is more than two times the maximum health
+                    // allowed for other players, limit health to this value
+                    health = 2 * MaxHealth;
+                }
+                else
+                {
+                    // Set health (if it is between 0 and 2 * MaxHealth)
+                    health = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This constructor initializes player health and weapon with values
+        /// specified by the code who creates a new player instance.
+        /// </summary>
+        /// <param name="health">Initial player health.</param>
+        /// <param name="weapon">Initial player weapon.</param>
+        public Berserker(float health, string weapon) : base(health, weapon)
+        {
+            // All the stuff is done by the base constructor
+        }
+
+        /// <summary>
+        /// Attack an enemy.
+        /// </summary>
+        /// <param name="enemy">Enemy to attack.</param>
+        public override void Attack(FNPlayer enemy)
+        {
+            // Berserker causes 20 damage
+            enemy.TakeDamage(20);
+        }
+    }
+}
---
+++ Aula02/Exercicio10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    /// <summary>
+    /// This class tests the concrete Fortnite player classes.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program starts here.
+        /// </summary>
+        /// <param name="args">Not used.</param>
+        public static void Main(string[] args)
+        {
+
+            // Create instances of each player class
+            FNPlayer p1 = new Berserker(150, "Pickaxe");
+            FNPlayer p2 = new Demolitionist(100);
+
+            // Info before the fight beging
+            Console.WriteLine(" == Before the fight begins == ");
+            PrintPlayerInfo(p1);
+            PrintPlayerInfo(p2);
+            Console.WriteLine();
+
+            // Do some battles
+            PerformAttack(p1, p2);
+            PerformAttack(p2, p1);
+            PerformAttack(p1, p2);
+            PerformAttack(p1, p1);
+            PerformAttack(p2, p1);
+            PerformAttack(p2, p1);
+            PerformAttack(p2, p1);
+
+        }
+
+        /// <summary>
+        /// Static method which performs an attack.
+        /// </summary>
+        /// <param name="p1">Player who performs the attack.</param>
+        /// <param name="p2">Player who suffers the attack.</param>
+        private static void PerformAttack(FNPlayer p1, FNPlayer p2)
+        {
+            // Determine player classes
+            string p1class = p1.GetType().Name;
+            string p2class = p1 != p2 ? p2.GetType().Name : "himself";
+
+            // Perform attack and show info
+            Console.WriteLine($" == {p1class} attacks {p2class} == ");
+            p1.Attack(p2);
+            PrintPlayerInfo(p1);
+            PrintPlayerInfo(p2);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Helper method to print player info.
+        /// </summary>
+        /// <param name="player"></param>
+        private static void PrintPlayerInfo(FNPlayer player)
+        {
+            Console.WriteLine("{0} is {1} => Health is {2} and Weapon is {3}",
+                player.GetType().Name,
+                player.Alive ? "alive" : "dead",
+                player.Health,
+                player.Weapon);
+        }
+    }
+}
