3f50e8ff8f888da0d836db22351b061877cf3765
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-10-12 18:49:38 +0100

    Adicionar exercício 3 da aula 3

---
+++ Aula03/Exercicio03/Player.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    /// <summary>
+    /// Class which represents a player, which has a name and a score.
+    /// </summary>
+    public class Player : IHasScore, IComparable<Player>
+    {
+        /// <summary>
+        /// Instance variable that contains the actual score and supports the
+        /// Score property.
+        /// </summary>
+        private int score;
+
+        /// <summary>
+        /// The Score property, in accordance with the IHasScore interface.
+        /// For this class, Score can never be less than zero.
+        /// </summary>
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                // Make sure score is never less than zero
+                if (value < 0)
+                {
+                    score = 0;
+                }
+                else
+                {
+                    score = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Auto-implemented property that represents the player's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This method returns true if another instance of type IHasScore
+        /// contains the same score as the current instance. This method is
+        /// required because Player implements <c>IEquatable</c> (via
+        /// <see cref="IHasScore"/>).
+        /// </summary>
+        /// <param name="other">
+        /// An instance of a class that implements the IHasScore interface.
+        /// </param>
+        /// <returns>True if both instances have the same score.</returns>
+        public bool Equals(IHasScore other)
+        {
+            if (other == null) return false;
+            return Score == other.Score;
+        }
+
+        /// <summary>
+        /// This method compares two players according to the
+        /// <see cref="IComparable{T}"/> interface. Players with a higher
+        /// score come before players with a lower score.
+        /// </summary>
+        /// <param name="other">
+        /// The player which will be compared to the current one.
+        /// </param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being
+        /// compared. If less than zero, the current player comes before
+        /// <paramref name="other"/>; if zero, both players are in the same
+        /// position; if greater than zero, the current player comes after
+        /// <paramref name="other"/>.
+        /// </returns>
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 0;
+            return other.Score - Score;
+        }
+
+        /// <summary>
+        /// This method overrides <see cref="object.ToString()"/>.
+        /// </summary>
+        /// <returns>A string representing the player state.</returns>
+        public override string ToString()
+        {
+            return $"My name is {Name} and my score is {Score}";
+        }
+    }
+}
---
+++ Aula03/Exercicio03/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio03
+{
+    /// <summary>
+    /// Test how to sort stuff in a collection.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program starts here.
+        /// </summary>
+        /// <param name="args">Not used.</param>
+        public static void Main(string[] args)
+        {
+            // The option selected by the user
+            string option;
+            // The list of players, initially empty
+            List<Player> players = new List<Player>();
+
+            // Main menu loop
+            do
+            {
+                // Get option from user
+                option = Menu();
+                // Check option
+                switch (option)
+                {                    
+                    case "i": // Insert player option
+                        InsertPlayer(players);
+                        break;
+                    case "l": // List players option
+                        ListPlayers(players);
+                        break;
+                    case "q": // Quit
+                        break;
+                    default: // Unknown option
+                        Console.WriteLine("Unknown option!");
+                        break;
+                }
+                // Keep looping until user quits
+            } while (option != "q");
+
+        }
+
+        /// <summary>
+        /// Show main menu and get user option.
+        /// </summary>
+        /// <returns>User option.</returns>
+        private static string Menu()
+        {
+            // Show menu
+            Console.WriteLine("------------------");
+            Console.WriteLine("(I)nsert player");
+            Console.WriteLine("(L)ist players");
+            Console.WriteLine("(Q)uit");
+            Console.WriteLine("------------------");
+            // Ask and return player option, removing whitespace before and
+            // after the option and converting the option to lowercase
+            return Console.ReadLine().Trim().ToLower();
+        }
+
+        /// <summary>
+        /// Insert a player in the list.
+        /// </summary>
+        /// <param name="players">The list of players.</param>
+        private static void InsertPlayer(List<Player> players)
+        {
+            // Required local variables
+            string playerName, playerScoreStr;
+            int playerScore;
+            Player player;
+
+            // Get player name
+            Console.Write("Player name  : ");
+            playerName = Console.ReadLine();
+
+            // Get player score, must be valid integer
+            while (true)
+            {
+                Console.Write("Player score : ");
+                playerScoreStr = Console.ReadLine();
+
+                // Check if score is valid integer
+                if (int.TryParse(playerScoreStr, out playerScore))
+                {
+                    // If so, get out of loop
+                    break;
+                }
+                // If not, show error message
+                Console.WriteLine("Invalid integer, please try again");
+            };
+
+            // Instantiate new player (using property initialization syntax)
+            player = new Player() { Name = playerName, Score = playerScore };
+
+            // Add player to list
+            players.Add(player);
+        }
+
+        /// <summary>
+        /// List all players, sorted by score.
+        /// </summary>
+        /// <param name="players">The players list.</param>
+        private static void ListPlayers(List<Player> players)
+        {
+            // Sort players
+            players.Sort();
+
+            // Show player list
+            Console.WriteLine("==== Player List ====");
+            foreach (Player player in players)
+            {
+                // We're using the Player.ToString() method to show player info
+                Console.WriteLine(player);
+            }
+            Console.WriteLine("=====================");
+        }
+    }
+}
