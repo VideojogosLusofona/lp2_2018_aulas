6d95707574f367520ecfe18c4c90d1e264700f45
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-01-09 11:20:31 +0000

    Adicionar exercício 5 da aula 13

---
+++ Aula13/Exercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                #warning We're running in Debug mode
+            #elif RELEASE
+                #error Code not ready for Release!
+            #else
+                #error Unknown build configuration!
+            #endif
+        }
+    }
+}
